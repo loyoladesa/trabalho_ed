766d4280fea33250378f1c819e9a67c00af1a894
loyoladesa <49080717+loyoladesa@users.noreply.github.com>
2019-06-03 07:48:49 -0300

    Update main.c

--- teste/main.c
+++ teste/main.c
@@ -4,10 +4,24 @@
 #include "arv_gen.h"
 #include "figura.h"
 
-static int* cmp_cod_figura(void* chave,void* info){
-	int* cod = chave;
+static int cmp_cod_figura(void* chave,void* info){
+	int cod = chave;
 	TFIG* f = (TFIG*)info;
 	return (cod - f->cod);
+}
+
+static int insere_figura(TAG* a, int cod,int cod_pai,char nome[121],float dim[3]){
+	TFIG* f = (TFIG*)malloc(sizeof(TFIG));
+  f->cod = cod;
+  f->cod_pai = cod_pai;
+  strcpy(f->nome, nome);
+	for(int i=0;i<3;i++){
+		f->dim[i] = dim[i];
+	}
+	
+	agen_insere(a,f->cod_pai,f);
+
+	return 3;
 }
 
 int main(void) {
@@ -18,5 +32,13 @@ int main(void) {
   strcpy(f->nome, "TRI");
 
   imprime(f);
+
+
+  TAG* a = agen_cria(cmp_cod_figura);
+  int x = insere_figura(a,1,0,"TRI",d);
+  if(x==3)printf("sucesso!\n");
+
+  printf("Cod = %d\n",a->raiz->cod);
+  printf("fim!\n");
   return 0;
 }
